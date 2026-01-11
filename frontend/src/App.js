35bc682b44c8acb344e0da459b98a89ab4065d21
anjaliofficial <bistaanjali1415@gmail.com>
2026-01-11 23:03:38 +0545

    path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";       // <-- component import
+import Navbar from "./components/navbar.jsx";       // <-- component import
 import "./components/Navbar.css";               // <-- CSS import
-import Home from "./pages/Home";
+import Home from "../src/pages/Home.";
 import Result from "./pages/Result";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
