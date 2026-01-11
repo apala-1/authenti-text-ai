f83c59bcd07d810fa92697ee86ad5232cc01c047
anjaliofficial <bistaanjali1415@gmail.com>
2026-01-11 23:08:53 +0545

    path fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar.jsx";       // <-- component import
-import "./components/Navbar.css";               // <-- CSS import
-import Home from "../src/pages/Home.";
+import Navbar from "./components/navbar";      // matches Navbar.jsx
+import Home from "./pages/Home";               // matches Home.jsx
 import Result from "./pages/Result";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import "./components/Navbar.css";
+
+
 
 function App() {
   return (
